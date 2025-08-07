7ddf15aa7c23c09a5b33b3253b1252d97592f8c4
Aditya Sahni <sahniaditya951@gmail.com>
2025-08-07 10:09:30 +0530

    update api url in Config.js

--- frontend/js/Config.js
+++ frontend/js/Config.js
@@ -1,2 +1,2 @@
 // const API_URL = "https://vsat-crm-backend.onrender.com";
-const API_URL = "https://vsat-crm-production.up.railway.app/";
+const API_URL = "https://vsat-crm-production.up.railway.app";
